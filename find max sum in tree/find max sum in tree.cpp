cd1ae6fb64e74c2f7d805cf01071e05c0239949e
compiletimeterror <56031091+compiletimeterror@users.noreply.github.com>
2020-10-01 00:16:45 +0530

    Add files via upload

---
+++ find max sum in tree/find max sum in tree.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int findMaxSum(node* root, int& ans){
+    if(root == NULL)
+        return 0;
+    if(root->left == NULL and root->right == NULL)
+        return root->data;
+   //complete the code
+}
